c13d0a67bc8f08cc83bd8c208bcaab81b0ebc4e4
Ananya Garg <62787867+Ananyaa03@users.noreply.github.com>
2021-06-10 22:05:12 +0530

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,6 +1,6 @@
 import requests
 
-api_address="http://api.openweathermap.org/data/2.5/weather?q=Angul&appid=f895e5c66cd4155dd875e080e7514226"
+api_address="http://api.openweathermap.org/data/2.5/weather?q=Angul&appid=yourapikey"
 json_data=requests.get(api_address).json()
 
 
